8728d2dbee558b78dd4afe8a6232ceca941d35e3
LucasHuertas <62759495+LucasHuertas@users.noreply.github.com>
2020-06-14 21:42:54 -0300

    Delete Bronze.java

--- Bronze.java
+++
@@ -1,8 +0,0 @@
-package PaquetePrincipal;
-
-public class Bronze extends Avion{
-
-    public Bronze(int capConbustible, float costoKm, int capPasajeros, int velocidadMax, String propulsion){
-        super(capConbustible, costoKm, capPasajeros, velocidadMax, propulsion);
-    }
-}
